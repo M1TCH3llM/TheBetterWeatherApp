9130c8468e49ccf754858cbeb878e4db96781c23
Mitchell.Morgan <mitchemorgan@gmail.com>
2023-09-06 17:09:14 -0600

    added JS to display main weather feature.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,12 @@
-var searchButton = document.getElementById("#searchButton");
+var searchButton = document.getElementById("searchButton");
+var searchInput = document.getElementById("searchT");
+var cityNameEl = document.getElementById("cityName");
+var currentTempEl = document.getElementById("currentTemp");
+var currentHumidEl = document.getElementById("currentHumid");
+var currentWindEl = document.getElementById("currentWind");
 
-function getWeather() {
-  var requestWeather =
-    "https://api.openweathermap.org/data/2.5/weather?q=Denver,us&APPID=6125957e3b746825efbf44ae31af7452";
+function getWeather(city) {
+  var requestWeather = `https://api.openweathermap.org/data/2.5/weather?q=${city},us&units=imperial&APPID=6125957e3b746825efbf44ae31af7452`;
 
   fetch(requestWeather)
     .then(function (response) {
@@ -10,9 +14,33 @@ function getWeather() {
     })
     .then(function (data) {
       console.log(data);
+
+      saveSearch(data.name);
+      displayWeather(data);
     });
 }
 
-getWeather();
+function displayWeather(data) {
+  cityNameEl.textContent = data.name;
+  currentTempEl.textContent = `Temp: ${data.main.temp}°`;
+  currentHumidEl.textContent = data.main.humidity;
+  currentWindEl.textContent = data.wind.speed;
+}
 
-// // searchButton.addEventListener("click", getWeather());
+function saveSearch (cityName) {
+    var pastSearch = JSON.parse(localStorage.getItem("cities")) || [];
+    if (!pastSearch.includes(cityName)){
+        pastSearch.push(cityName);
+        localStorage.setItem("cities", JSON.stringify(pastSearch));
+    }
+
+
+}
+
+searchButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  var cityVal = searchInput.value;
+  getWeather(cityVal);
+});
+
+
